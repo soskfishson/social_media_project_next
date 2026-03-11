18abaff075c930c0c9943551ed683c53cffa7e43
Soskfishson <fox25055@gmail.com>
2026-03-11 11:53:10 +0300

    feat: transfered error and 404 pages to the next project

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,24 +2,22 @@
 
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
-import NotFound from '@/assets/Error404.svg';
+import Error from '@/assets/Error.svg';
 import { useTranslation } from 'react-i18next';
 import './error.css';
 
-const NotFoundPage = () => {
+export default function ErrorPage() {
     const { t } = useTranslation();
     return (
-        <div className="error-page  page-transition-wrapper">
+        <div className="error-page page-transition-wrapper">
             <Header variant="simple" />
             <main className="error-page-container">
-                <NotFound />
+                <Error />
                 <p className="error-message" data-testid="error-message">
-                    {t('errors.notFound')}
+                    {t('errors.generic')}
                 </p>
             </main>
             <Footer />
         </div>
     );
-};
-
-export default NotFoundPage;
+}
